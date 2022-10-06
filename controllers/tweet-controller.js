4b46e7927288d764940f205e586bd9aa47850931
Ckai <ckai.trans@gmail.com>
2022-10-06 09:26:12 +0800

    feat: add GET/api/tweet

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -4,6 +4,15 @@ const tweetController = {
   getTweets: (req, res, next) => {
   },
   getTweet: (req, res, next) => {
+    return Tweet.findByPk(req.params.id, {
+      include: [{ model: User }],
+      nest: true,
+      raw: true
+    })
+    .then(tweet => {
+      return res.json(tweet)
+    })
+    .catch(err => next(err))
   },
   postTweet:(req, res, next) => {
   },
