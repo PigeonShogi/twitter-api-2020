4e9e2b821ac3fc6e1d65737b1935b5643761d991
Ckai <ckai.trans@gmail.com>
2022-10-11 00:29:48 +0800

    modify: adjust tweets-seed-file 2

--- seeders/20221007143703-tweets-seed-2.js
+++ seeders/20221007143703-tweets-seed-2.js
@@ -14,13 +14,13 @@ module.exports = {
       },
       {
       user_id: users[ users.length - 2 ].id,
-      description: '這是種子檔產生的，用以凸顯時間順序的假文，來自未來的文章。',
+      description: '這也是種子檔產生的，用以凸顯時間順序的假文，來自未來的文章。',
       created_at: '2222-12-02 05:53:55',
       updated_at: '2222-12-02 05:53:55',
       },
       {
       user_id: users[ users.length - 3 ].id,
-      description: '這是種子檔產生的，用以凸顯時間順序的假文，來自未來的文章。',
+      description: '這還是種子檔產生的，用以凸顯時間順序的假文，來自未來的文章。',
       created_at: '2222-12-03 05:53:55',
       updated_at: '2222-12-03 05:53:55',
       }
